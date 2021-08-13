8ed05df2b7a6f5eb0845092b4e08c05518d2f165
Laura Tolosa <lauratolosa96@gmail.com>
2021-08-13 12:15:37 -0500

    evitar usar Fragment

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react'
 
 const PrimerComponente = () => {
     return (
-        <Fragment>
+        <>
             <h1>Hola Mundo</h1>
             <p> Mi primera App</p>
-        </Fragment>
+        </>
     )
 }
 
