505e25104a221308bd66905547c02098c8224da2
Laura Tolosa <lauratolosa96@gmail.com>
2021-08-30 15:20:37 -0500

    extraer una funcion y consumirla solo despues de un evento

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -2,11 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Contador = ({value}) => {
+
+    //handleAdd 
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return (
         <>
             <h1>Contador Componente</h1>
             <h2>{value}</h2>
-            <button onClick={(e) => {console.log(e)}}>+1</button>
+            <button onClick={handleAdd}>+1</button>
             {/*Hay diferentes eventos sinteticos en react para mas
             informacion ir a https://es.reactjs.org/docs/events.html*/ }
         </>
